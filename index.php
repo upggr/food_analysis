57d03e98dd6d54f8dbeb2edc72eb9ebcfefb1d57
Ioannis Kokkinis <info@upg.gr>
2018-07-28 20:06:49 +0300

    Updated index.php.

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-test
+<input id="userphoto" type="file" accept="image/*;capture=camera">
+
+<script>
+
+var myInput = document.getElementById('userphoto');
+
+function sendPic() {
+    var file = myInput.files[0];
+console.log(file);
+    // Send file here either by adding it to a `FormData` object
+    // and sending that via XHR, or by simply passing the file into
+    // the `send` method of an XHR instance.
+}
+
+myInput.addEventListener('change', sendPic, false);
+</script>
+ 
